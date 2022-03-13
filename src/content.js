fbbc078a90bc318bdf5d73267e02368631d0063f
segre <segrehouck5458@gmail.com>
2022-03-13 14:53:19 +0900

    メニューバーにブックマークボタンを追加

--- src/content.js
+++ src/content.js
@@ -1,10 +1,14 @@
 jQuery(function ($) {
   //$(".title").css('background-color', 'rgb(255,0,0)');
   $(".info").append('<span data-v-f3ac38c0 class="favorite styled" id="button">★</span>');
-  
-  $(".favorite").click(function(){
+
+  $(".favorite").click(function () {
     $(this).toggleClass('isActive');
   })
 
-  $(".loginMenu").append('<li data-v-69b3dcd9><a data-v-69b3dcd9 href=/bookmark>"ブックマーク"</a></li>');
+  $(".menuBtn").click(function () {
+    if (!($('#BtnBookMark').length)) {
+      $(".loginMenu").append('<li data-v-69b3dcd9><a data-v-69b3dcd9 href=/bookmark id="BtnBookMark">★ ブックマーク</a></li>');
+    }
+  })
 });
