d93914534200dd13910ba2f64bf9f4295df9bdc7
segre <segrehouck5458@gmail.com>
2022-03-28 23:21:17 +0900

    bookmark時のclass変移を修正

--- src/content.js
+++ src/content.js
@@ -2,6 +2,9 @@ jQuery(function ($) {
   $(".info").append('<span data-v-f3ac38c0 class="favorite styled" id="button">★</span>');
 
   $(".favorite").click(function () {
+    if ($(this).css("color") == "rgb(255, 165, 0)") {
+      $(".isActive").parent().parent().prop("class", "item");
+    }
     $(this).toggleClass('isActive');
     if ($(this).css("color") == "rgb(255, 165, 0)") {
       $(".isActive").parent().parent().prop("class", "item fav");
@@ -11,12 +14,12 @@ jQuery(function ($) {
   $(".menuBtn").click(function () {
     if (!($('#BtnBookMark').length)) {
       $(".loginMenu").append('<li data-v-69b3dcd9 id="bookMark"><button data-v-69b3dcd9 id="BtnBookMark">★ ブックマーク</button></li>');
-      if ($(".item").prop("class") == "item fav") {
-        var refer = $(".fav > .title").prop("href");
-        var title = $(".fav > .title").text();
-        var content = '<a data-v-69b3dcd9 href=' + refer + '>' + title + '</a>';
-        $("#bookMark").append(content);
-      }
+      var refer = $(".fav > .title").prop("href");
+      var title = $(".fav > .title").text();
+      console.log(refer[0]);
+      console.log("hoge");
+      var content = '<a data-v-69b3dcd9 href=' + refer + '>' + title + '</a>';
+      $("#bookMark").append(content);
     }
   })
 });
