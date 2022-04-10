87ce671b94ba94dc2cc8ca9d7e613de530e95729
segre <segrehouck5458@gmail.com>
2022-04-10 22:55:50 +0900

    すでにブックマークされているものはjsonに追加

--- src/content.js
+++ src/content.js
@@ -1,18 +1,23 @@
 jQuery(function ($) {
+  let obj;
   // 星マークを追加
   $(".info").append('<span data-v-f3ac38c0 class="favorite styled" id="button">★</span>');
   // ブックマーク済みの記事をブックマーク
   let title = "";
   for (let i = 1; i <= 100; i++) {
     chrome.storage.local.get(String(i), function (result) {
+      // TO DO:titleがundifinedのときのbreak処理
       try {
         title = result[i].title;
         $("a:contains(" + title + ")").parent().find(".info").find(".favorite").toggleClass('isActive');
+        $("a:contains(" + title + ")").parent().prop("class", "item fav");
       }
       catch (error) {
       }
     })
   }
+
+
 
   let cnt = 0;
   $(".favorite").click(function () {
@@ -24,7 +29,6 @@ jQuery(function ($) {
       $(this).parent().parent().prop("class", "item fav");
     }
     // bookmark用json生成
-    // TO DO: bookmarkを外したときjsonから削除
     cnt = 0;
     let data = "{";
     $(".fav").each(function () {
@@ -38,7 +42,7 @@ jQuery(function ($) {
     })
     let json = data.slice(0, -1);
     json += "}";
-    let obj = JSON.parse(json);
+    obj = JSON.parse(json);
     console.log(obj);
     chrome.storage.local.set(obj, function () {
     });
